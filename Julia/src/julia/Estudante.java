f7f82808423df87ebf7ef6078bc408286df874b1
JuliaKawano <kawanojulia490@gmail.com>
2026-03-17 11:18:07 -0300

    Update Estudante.java

--- Julia/src/julia/Estudante.java
+++ Julia/src/julia/Estudante.java
@@ -1,34 +1,116 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package julia;
+package estudante;
+
 import java.util.Date;
 
+/**
+ *
+ * @author aluno
+ */
 public class Estudante {
+
+    public String cpf;
     public String nome;
     public Date dataNascimento;
-    public String cpf;
     public char genero;
-    public String matricula;
+    public int matricula;
     public String email;
-   
+    public int telefone;
+    public int anoDeIngresso;
+    public int semestreIngresso;
+    public String situacaoAcademica; 
+
     public Estudante(){
         this.nome = "Nobody";
     }
-   
-    public int obterIdade (Date hoje) {
-        int idade =0;
-        // Lógica para calcular idade.
-        return idade;
+       
+    public int obterIdade(Date hoje) {
+    int idade = 0;
+    return idade;
     }
-    public String getNome(){
-        obterIdade (new Date());
+    public String getNome (){
         return this.nome;
     }
-    
+           
     public void setNome (String _nome){
-        this.nome=_nome;
+        this.nome = _nome;
     }
-}
+           
+    public Date getDate (){
+        return this.dataNascimento;
+    }
+           
+    public void setDate (Date _dataNascimento){
+        this.dataNascimento = _dataNascimento;
+    }
+   
+    public String getCpf (){
+        return this.cpf;
+    }
+           
+    public void setCpf (String _cpf){
+        this.cpf = _cpf;
+    }
+   
+    public char getGenero (){
+        return this.genero;
+    }
+           
+    public void setGenero (char _genero){
+        this.genero = _genero;
+    }
+   
+    public int getMatricula (){
+        return this.matricula;
+    }
+           
+    public void setMatricula (int _matricula){
+        this.matricula = _matricula;
+    }
+   
+    public String getEmail (){
+        return this.email;
+    }
+    
+    public void setEmail(String _email){
+        this.email = _email;
+        
+    }
+            
+    public int getTelefone () {
+        return this.telefone;
+    }
+    
+    public void setTelefone (int _telefone){
+        this.telefone = _telefone;
+    }
+    
+    public int getAnoDeIngresso() {
+        return this.anoDeIngresso;
+    }
 
+    public void setAnoDeIngresso(int _anoDeIngresso) {
+        this.anoDeIngresso = _anoDeIngresso;
+    }
+
+    public int getSemestreIngresso() {
+        return this.semestreIngresso;
+    }
+
+    public void setSemestreIngresso(int _SemestreIngresso) {
+        this.semestreIngresso = _SemestreIngresso;
+    }
+    
+    public String getsituacaoAcademica() {
+        return this.situacaoAcademica;
+    }
+
+    public void setsituacaoAcademica(String _situacaoAcademica) {
+        this.situacaoAcademica = _situacaoAcademica;
+    }
+    
+    
+}
